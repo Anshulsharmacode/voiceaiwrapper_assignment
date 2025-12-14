2469a1f55a408e2c7ff456a16e70b4e7e605be8c
Anshulsharmacode <anshulsharma8386@gmail.com>
2025-12-14 23:59:35 +0530

    Add Task configuration to admin panel

--- Backend/task/admin.py
+++ Backend/task/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'assignee_email', 'due_date', 'created_at')
+    list_filter = ('status', 'created_at', 'project', 'due_date')
+    search_fields = ('title', 'description', 'assignee_email', 'project__name')
+    readonly_fields = ('created_at',)
+    date_hierarchy = 'due_date'
